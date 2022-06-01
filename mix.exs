f0ebdfc7c67adc120a9720bc54bf51643dc6268d
Michał Śledź <michalsledz34@gmail.com>
2022-06-01 13:48:49 +0200

    Bump version to 0.8.0

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule GuideExDoc.MixProject do
     [
       app: :membrane_framework_guide,
       name: "Membrane Guide",
-      version: "0.7.0",
+      version: "0.8.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       docs: docs(),
