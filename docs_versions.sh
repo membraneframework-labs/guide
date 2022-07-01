8d5e06587ffda75e078a6a9b32404ec8c9edb549
Piotrek Zyla <piotr.zyla@swmansion.com>
2022-07-01 15:25:14 +0200

    fixes to Dockerfile and script

--- docs_versions.sh
+++ docs_versions.sh
@@ -2,12 +2,13 @@
 
 for version in 3 4 5 6 7 
 do
- git clone --branch v0.$version  https://github.com/membraneframework/guide.git v0.$version \
+    mkdir -p guide/v0.$version \
+    && git clone --branch v0.$version  https://github.com/membraneframework/guide.git v0.$version \
     && cd v0.$version \
     && mix local.rebar --force \
     && mix local.hex --force \
     && mix deps.get \
     && MIX_ENV=dev mix docs && mix docs 2>&1 | (! grep -q "warning:") \
+    && cp -R doc/* ../guide/v0.$version/. \
     && cd ../
 done
-
