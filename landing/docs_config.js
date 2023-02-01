b7bc6d1b4ae46deab2bf9c570b6557471b31b18d
DominikWolek <dominik.wolek@swmansion.com>
2023-02-01 11:27:33 +0100

    Fix wrong versioning (#75)

--- landing/docs_config.js
+++ landing/docs_config.js
@@ -1,7 +1,7 @@
 var baseUrl = "https://membrane.stream/guide"
 var versionNodes = [
   { version: "v0.9.0", url: baseUrl + "/v0.9" },
-  { version: "v0.9.0", url: baseUrl + "/v0.8" },
+  { version: "v0.8.0", url: baseUrl + "/v0.8" },
   { version: "v0.7.0", url: baseUrl + "/v0.7" },
   { version: "v0.6.0", url: baseUrl + "/v0.6" },
   { version: "v0.5.0", url: baseUrl + "/v0.5" },
