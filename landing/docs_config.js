b046a2a32c1be9705e7ef50030592a9917dd5d0d
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-02-01 10:56:19 +0100

    Add versions 0.8 and 0.9 do scripts

--- landing/docs_config.js
+++ landing/docs_config.js
@@ -1,8 +1,10 @@
 var baseUrl = "https://membrane.stream/guide"
 var versionNodes = [
-  {version: "v0.7.0", url: baseUrl + "/v0.7"},
-  {version: "v0.6.0", url: baseUrl + "/v0.6"},
-  {version: "v0.5.0", url: baseUrl + "/v0.5"},
-  {version: "v0.4.0", url: baseUrl + "/v0.4"},
-  {version: "v0.3.0", url: baseUrl + "/v0.3"}
+  { version: "v0.9.0", url: baseUrl + "/v0.9" },
+  { version: "v0.9.0", url: baseUrl + "/v0.8" },
+  { version: "v0.7.0", url: baseUrl + "/v0.7" },
+  { version: "v0.6.0", url: baseUrl + "/v0.6" },
+  { version: "v0.5.0", url: baseUrl + "/v0.5" },
+  { version: "v0.4.0", url: baseUrl + "/v0.4" },
+  { version: "v0.3.0", url: baseUrl + "/v0.3" }
 ]
