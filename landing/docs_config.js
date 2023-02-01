ff39e6ad5d7d682755d6760c28a1b6d45d316582
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-02-01 11:20:56 +0100

    Add v0.8 to landing files

--- landing/docs_config.js
+++ landing/docs_config.js
@@ -1,10 +1,11 @@
 var baseUrl = "https://www.membraneframework.org/guide"
 var versionNodes = [
-  {version: "v0.7.0", url: baseUrl + "/v0.7/"},
-  {version: "v0.6.0", url: baseUrl + "/v0.6/"},
-  {version: "v0.5.0", url: baseUrl + "/v0.5/"},
-  {version: "v0.4.0", url: baseUrl + "/v0.4/"},
-  {version: "v0.3.0", url: baseUrl + "/v0.3/"},
-  {version: "v0.2.0", url: baseUrl + "/v0.2/"},
-  {version: "v0.1.0", url: baseUrl + "/v0.1/"}
+  { version: "v0.8.0", url: baseUrl + "/v0.8/" },
+  { version: "v0.7.0", url: baseUrl + "/v0.7/" },
+  { version: "v0.6.0", url: baseUrl + "/v0.6/" },
+  { version: "v0.5.0", url: baseUrl + "/v0.5/" },
+  { version: "v0.4.0", url: baseUrl + "/v0.4/" },
+  { version: "v0.3.0", url: baseUrl + "/v0.3/" },
+  { version: "v0.2.0", url: baseUrl + "/v0.2/" },
+  { version: "v0.1.0", url: baseUrl + "/v0.1/" }
 ]
