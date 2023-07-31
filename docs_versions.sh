44d1025b3cbcc8652a729083e37586a8353d8ca9
blazpie <blazej.pierzak@gmail.com>
2023-07-31 12:40:42 +0200

    Update docs_versions.sh

--- docs_versions.sh
+++ docs_versions.sh
@@ -2,13 +2,13 @@
 
 for version in 3 4 5 6 7 8 9
 do
-    mkdir -p guide/v0.$version \
+    mkdir -p /app/guide/v0.$version \
+    && cd /app
     && git clone --branch v0.$version  https://github.com/membraneframework/guide.git v0.$version \
-    && cd v0.$version \
+    && cd /app/v0.$version \
     && mix local.rebar --force \
     && mix local.hex --force \
     && mix deps.get \
     && MIX_ENV=dev mix docs && mix docs 2>&1 | (! grep -q "warning:") \
-    && cp -R doc/* ../guide/v0.$version/. \
-    && cd ../
+    && cp -R /app/v0.$version/doc/* /app/guide/v0.$version
 done
